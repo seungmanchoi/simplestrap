5b8730001ef68f84f225d7c67f9dc069e13f9a61
seungmanchoi <blueng.choi@gmail.com>
2017-04-06 17:13:30 +0900

    theme info 생성

---
+++ theme/Theme.php
@@ -0,0 +1,9 @@
+<?php
+namespace Xpressengine\Plugins\Simplestrap\Theme;
+
+use Xpressengine\Theme\GenericTheme;
+
+class Theme extends GenericTheme
+{
+    protected static $path = 'simplestrap/theme';
+}
